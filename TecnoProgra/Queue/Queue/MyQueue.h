bcfceb1f370b30f6916cfb98d3bf2f4934f0bd77
RogerAlamanac <roger.alamanac@enti.cat>
2024-05-09 14:12:24 +0200

    qUEUEEEEE

--- TecnoProgra/Queue/Queue/MyQueue.h
+++ TecnoProgra/Queue/Queue/MyQueue.h
@@ -1,69 +1,87 @@
 #pragma once
-
+#include <iostream>
 class MyQueue
 {
 private:
-	struct Node {
-		int value;
-		Node* next;
-	};
+    struct Node
+    {
+        int value;
+        Node* next;
+    };
 
-	Node* m_first;
-	Node* m_last;
-	size_t m_size = 0;
-
+    Node* m_first;
+    Node* m_last;
+    size_t m_size = 0;
 public:
-	MyQueue() {
-		m_first = nullptr;
-		m_last = nullptr;
-		m_size = 0;
-	}
+    MyQueue()
+    {
+        m_first = nullptr;
+        m_last = nullptr;
+        m_size = 0;
+    }
 
-	void Push(int value) {
-		//Create node
-		Node* node = new Node();
-		node->value = value;
+    void Push(int value)
+    {
+        Node* node = new Node();
+        node->value = value;
 
-		//Update first and last
-		if (m_first == nullptr) {
+        if (m_first = nullptr)
+        {
+            m_first = node;
+            m_last = node;
+        }
+        else
+        {
+            // Update last
+            m_last->next = node;
+            m_last = m_last->next;
+            // Opcion 2: last = node
+        }
+    }
 
-		}
-		else {
-			// Update last
-			m_last->next = node;
-			m_last = node;
-		}
-		m_size++;
-	}
+    int Pop()
+    {
+        if (m_first == nullptr)
+        {
+            return -1;
+        }
+        Node* nodeToRemove = m_first;
+        int valueToRemove = nodeToRemove->value;
 
-	int Pop() {
-		if (m_first == nullptr) {
-			return -1; //Invalid
-		}
+        delete nodeToRemove;
+        --m_size;
+    }
+    int Back()
+    {
+        if (m_last == nullptr)
+        {
+            return -1;
+        }
+        return m_last->value;
+    }
 
-		Node* nodeToremove = m_first;
-		int valueToRemove = nodeToremove->value;
-	}
+    void Clear()
+    {
+        while (!IsEmpty())
+        {
+            Pop();
+        }
+    }
 
-	int Front() const {
-		if (m_first == nullptr) {
-			return -1;
-		}
-		return m_first->value;
-	}
+    bool IsEmpty() const
+    {
+        return m_size == 0;
+    }
 
-	int Back() const {
-		if (m_last == nullptr) {
-			return -1;
-		}
-		return m_last->value;
-	}
 
-	void Print() const {
-		Node* aux = m_first;
-		while (m_first != nullptr) {
-
-		}
-	}
+    void Print() const
+    {
+        Node* copyTopPointer = m_first;
+        while (copyTopPointer != nullptr)
+        {
+            std::cout << copyTopPointer->value << "-";
+            copyTopPointer = copyTopPointer->next;
+        }
+        std::cout << std::endl;
+    }
 };
-
